9f696067b95b618b1d500c47497c09155dcc62d2
Evgeniy Kovyryashkin <seventhstar@mail.ru>
2018-02-20 19:20:33 +0700

    simple strings fill

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,16 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import org.json.JSONException;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private TextView placeOfOriginTextView;
+    private TextView descriptionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,8 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        placeOfOriginTextView = findViewById(R.id.detail_place_of_origin_tv);
+        descriptionTextView = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -36,14 +44,21 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        Sandwich sandwich = null;
+        try {
+            sandwich = JsonUtils.parseSandwichJson(json, this);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
+
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +71,22 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        setEmptyTexts();
+        setTitle(sandwich.getMainName());
 
+        String placeOfOrigin = sandwich.getPlaceOfOrigin();
+        if (placeOfOrigin != null && !placeOfOrigin.isEmpty()) {
+            placeOfOriginTextView.setText(sandwich.getPlaceOfOrigin());
+        } else {
+            placeOfOriginTextView.setText(getString(R.string.detail_origin_not_set));
+        }
+
+        descriptionTextView.setText(sandwich.getDescription());
+
+    }
+
+    private void setEmptyTexts() {
+        setTitle("");
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,48 @@
 package com.udacity.sandwichclub.utils;
 
+import android.content.Context;
+
+import com.udacity.sandwichclub.R;
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json, Context context) throws JSONException {
+
+        JSONObject jsonObject = new JSONObject(json);
+
+        JSONObject rootObject = jsonObject.getJSONObject(
+                context.getString(R.string.json_key_root_name));
+
+        String mainName = rootObject.getString(
+                context.getString(R.string.json_key_main_name));
+
+        String placeOfOrigin = jsonObject.getString(
+                context.getString(R.string.json_key_place_of_origin));
+
+        String description = jsonObject.getString(
+                context.getString(R.string.json_key_description));
+
+        String imageUrl = jsonObject.getString(
+                context.getString(R.string.json_key_image));
+
+
+        List<String> ingredientsList = new ArrayList<>();
+        List<String> alsoKnownAsList = new ArrayList<>();
+
+        return new Sandwich(mainName,
+                alsoKnownAsList,
+                placeOfOrigin,
+                description,
+                imageUrl,
+                ingredientsList);
     }
+
 }
